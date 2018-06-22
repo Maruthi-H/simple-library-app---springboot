a0c1a7f83a64e5638d0834b5cc8e47d6db0ea3a8
Maruthi H <maruthi.rh94@gmail.com>
2018-06-22 15:28:56 +0530

    remove the redundent folder

--- spring5webapp/src/main/java/com/maruthi/spring5webapp/repositories/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.maruthi.spring5webapp.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.maruthi.spring5webapp.model.Book;
-
-public interface BookRepository extends CrudRepository<Book, Long>{
-	
-}
